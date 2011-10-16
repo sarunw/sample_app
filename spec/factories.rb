8926f613ce7b960b97885cff91dc9c060bb7d372
Sarun Wongpatcharapakorn <artwork.th@gmail.com>
2011-10-17 00:13:36 +0700

    User model with passwords, and user show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   "Sarun"          
+  user.email                  "art@gmail.com"  
+  user.password               "foobar"         
+  user.password_confirmation  "foobar"
+end
