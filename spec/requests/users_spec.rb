7378c4119a3785c84e24fd407920454a837705b2
Sarun Wongpatcharapakorn <artwork.th@gmail.com>
2011-10-17 22:54:47 +0700

    Finished exercise 9 (only first one)

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -44,11 +44,9 @@ describe "Users" do
     
     describe "failure" do
       
-      it "should not sign a uer in" do
-        visit signin_path
-        fill_in :email,         :with => ""
-        fill_in :password,      :with => ""
-        click_button
+      it "should not sign a user in" do
+        empty_user = User.new
+        integration_sign_in empty_user
         response.should have_selector("div.flash.error", :content => "Invalid")
       end
     end
@@ -56,10 +54,7 @@ describe "Users" do
     describe "success" do
       it "should sign a user in and out" do
         user = Factory(:user)
-        visit signin_path
-        fill_in :email,         :with => user.email
-        fill_in :password,      :with => user.password
-        click_button
+        integration_sign_in user
         controller.should be_signed_in
         click_link "Sign out"
         controller.should_not be_signed_in
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,6 +23,14 @@ Spork.prefork do
     def test_sign_in(user)
       controller.sign_in(user)
     end
+    
+    def integration_sign_in(user)
+      visit signin_path
+      fill_in :email,       :with => user.email
+      fill_in :password,    :with => user.password
+      click_button
+    end
+    
   end
 end
 
@@ -92,5 +100,12 @@ RSpec.configure do |config|
   
   def test_sign_in(user)
     controller.sign_in(user)
+  end
+  
+  def integration_sign_in(user)
+    visit signin_path
+    fill_in :email,       :with => user.email
+    fill_in :password,    :with => user.password
+    click_button
   end
 end
